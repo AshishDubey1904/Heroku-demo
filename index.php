ff4aaf685329da36340f0474d082e39e54bb3e29
AshishDubey1904 <55233498+AshishDubey1904@users.noreply.github.com>
2019-09-15 17:55:32 +0530

    Update with edit form

--- index.php
+++ index.php
@@ -74,14 +74,34 @@
                 <div class="modal-content">
                   <div class="modal-header">
                     <button type="button" class="close" data-dismiss="modal">&times;</button>
-                    <h4 class="modal-title">Modal Header</h4>
+                    <h4 class="modal-title">Edit Contact</h4>
                   </div>
-                  <div class="modal-body">
-                    <p>Some text in the modal.</p>
-                  </div>
-                  <div class="modal-footer">
-                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                  </div>
+                  <form method="post" action="<?php echo $_SERVER[PHP_SELF];?>">
+                    <div class="modal-body">
+                      <div class="row">
+                        <div class="col-md-12">
+                          <div class="form-group">
+                            <label>First Name:</label>
+                            <input type="text" name="first_name" value="<?php echo $row->firstname;?>">
+                          </div>
+                          <div class="form-group">
+                            <label>Last Name:</label>
+                             <input type="text" name="last_name" value="<?php echo $row->lastname;?>" required="required">
+                          </div>
+                          <div class="form-group">
+                            <label>Email:</label>
+                            <input type="email" name="email" value="<?php echo $row->email;?>">
+                          </div>
+                          
+                        </div>
+                      </div>
+                    </div>
+                    <div class="modal-footer">
+                      <input type="hidden" name="sfid" value="<?php echo $row->sfid;?>">
+                      <input type="submit" name="Edit_record" value="Update" class="btn btn-success">
+                      <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                    </div>
+                  </form>
                 </div>
 
               </div>
