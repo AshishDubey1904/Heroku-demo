ec116232b61cdc8998cbefe23d03e476ba0ec35c
AshishDubey1904 <55233498+AshishDubey1904@users.noreply.github.com>
2019-09-16 15:29:13 +0530

    fix delete

--- contact.php
+++ contact.php
@@ -79,12 +79,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' and isset($_POST['Edit_record'])){
 </nav>
 
 <?php
-  $insql = 'Delete from TABLE Postgrescontact';
+  $insql = 'Delete from Postgrescontact';
   $instmt = $pdo->prepare($insql);
   $instmt->execute();
-  //$insql1 = 'ALTER TABLE Postgrescontact ADD primaryid INT PRIMARY KEY AUTO_INCREMENT';
-  //$instmt1 = $pdo->prepare($insql1);
-  //$instmt1->execute(); 
+  $insql1 = 'ALTER TABLE Postgrescontact ADD primaryid INT PRIMARY KEY AUTO_INCREMENT';
+  $instmt1 = $pdo->prepare($insql1);
+  $instmt1->execute(); 
   //$insql = 'CREATE TABLE Postgrescontact AS (SELECT * FROM salesforce.contact WHERE 1=2);';
    //$instmt = $pdo->prepare($insql);
    //$instmt->execute();
