1c117a335afe4380ac017b4651818a1b192c162a
AshishDubey1904 <55233498+AshishDubey1904@users.noreply.github.com>
2019-09-16 16:12:03 +0530

    submit changes

--- contact.php
+++ contact.php
@@ -58,7 +58,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' and isset($_POST['Edit_record'])){
 ?>
 <body>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-  <a class="navbar-brand" href="<?php echo ROOT_URL; ?>">Heroku Test</a>
+  <a class="navbar-brand" href="<?php echo 'https://dmdelhi.herokuapp.com'; ?>">Heroku Test</a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
   <span class="navbar-toggler-icon"></span>
   </button>
@@ -66,7 +66,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' and isset($_POST['Edit_record'])){
   <div class="collapse navbar-collapse" id="navbarSupportedContent">
     <ul class="navbar-nav ml-auto">
       <li>
-        <a class="nav-link" href="<?php echo ROOT_URL; ?>">Home</a>
+        <a class="nav-link" href="<?php echo 'https://dmdelhi.herokuapp.com'; ?>">Home</a>
       </li>
       <li>
         <a class="nav-link" href="<?php echo ROOT_URL; ?>about">About</a>
