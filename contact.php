b535fe9e3d09d236b4ceb2235de9c93ad6b32e3e
AshishDubey1904 <55233498+AshishDubey1904@users.noreply.github.com>
2019-09-17 11:44:51 +0530

    update

--- contact.php
+++ contact.php
@@ -82,12 +82,12 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' and isset($_POST['Edit_record'])){
   $insql = 'Delete from Postgrescontact;';
   $instmt = $pdo->prepare($insql1);
   $instmt->execute(); 
-  $insql1 = 'ALTER TABLE Postgrescontact DROP COLUMN ID;';
-  $instmt1 = $pdo->prepare($insql1);
-  $instmt1->execute(); 
-  $insql2 = 'ALTER TABLE Postgrescontact ADD ID INT IDENTITY CONSTRAINT PK_Postgrescontact PRIMARY KEY CLUSTERED;';
-  $instmt2 = $pdo->prepare($insql2);
-  $instmt2->execute();
+  //$insql1 = 'ALTER TABLE Postgrescontact DROP COLUMN ID;';
+  //$instmt1 = $pdo->prepare($insql1);
+  //$instmt1->execute(); 
+  //$insql2 = 'ALTER TABLE Postgrescontact ADD ID INT IDENTITY CONSTRAINT PK_Postgrescontact PRIMARY KEY CLUSTERED;';
+  //$instmt2 = $pdo->prepare($insql2);
+  //$instmt2->execute();
   
     //$insql1 = 'ALTER TABLE Postgrescontact ADD COLUMN Primatycol INT AUTO_INCREMENT UNIQUE FIRST;';
   //$instmt1 = $pdo->prepare($insql1);
