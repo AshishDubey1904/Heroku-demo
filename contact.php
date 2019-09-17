41854585d4a43d90b261aa53a0680d6a0687412c
AshishDubey1904 <55233498+AshishDubey1904@users.noreply.github.com>
2019-09-17 11:51:14 +0530

    Update it with pk

--- contact.php
+++ contact.php
@@ -79,15 +79,15 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST' and isset($_POST['Edit_record'])){
 </nav>
 
 <?php
-  $insql = 'DELETE FROM Postgrescontact;';
-  $instmt = $pdo->prepare($insql);
-  $instmt->execute(); 
-  $insql1 = 'ALTER TABLE Postgrescontact DROP COLUMN ID;';
-  $instmt1 = $pdo->prepare($insql1);
-  $instmt1->execute(); 
-  //$insql2 = 'ALTER TABLE Postgrescontact ADD ID INT IDENTITY CONSTRAINT PK_Postgrescontact PRIMARY KEY CLUSTERED;';
-  //$instmt2 = $pdo->prepare($insql2);
-  //$instmt2->execute();
+  //$insql = 'DELETE FROM Postgrescontact;';
+  //$instmt = $pdo->prepare($insql);
+  //$instmt->execute(); 
+  //$insql1 = 'ALTER TABLE Postgrescontact DROP COLUMN ID;';
+  //$instmt1 = $pdo->prepare($insql1);
+  //$instmt1->execute(); 
+  $insql2 = 'ALTER TABLE Postgrescontact ADD ID INT IDENTITY CONSTRAINT PK_Postgrescontact PRIMARY KEY CLUSTERED;';
+  $instmt2 = $pdo->prepare($insql2);
+  $instmt2->execute();
   
     //$insql1 = 'ALTER TABLE Postgrescontact ADD COLUMN Primatycol INT AUTO_INCREMENT UNIQUE FIRST;';
   //$instmt1 = $pdo->prepare($insql1);
